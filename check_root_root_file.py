96aaff243ce51291a3fbf4a8a7349cfdb4ff09bd
katarinavelkov <katarina.velkov@gmail.com>
2025-07-22 18:24:57 +0200

    commit

--- check_root_root_file.py
+++ check_root_root_file.py
@@ -3,11 +3,17 @@ import numpy as np
 from sklearn.metrics import accuracy_score, roc_auc_score, precision_score, recall_score, f1_score
 import awkward as ak
 
-filename = 'predict_output/output.root'
-#filename = r'/eos/cms/store/group/cmst3/user/sesanche/SummerStudent/train_0p01/00ae10f7-1953-4002-8bb7-42c294122af1.root.root'
+filename = r'/eos/cms/store/group/cmst3/user/sesanche/SummerStudent/train_0p01/e0d8f59d-12d6-418f-809f-9b8d5d4265a3.root.root'
+
 with uproot.open(filename) as f:
-    events = f['Events']
-    print(events.keys())
+    top_level_keys = f.keys()
+    print("Top-level keys in the ROOT file:", top_level_keys)
+
+    events = f['GenObjectTree']
+    print("Branches in GenObjectTree:", events.keys())
+
+    isSignal = events["isSignal"].array()
+    print('uniques:', np.unique(isSignal))
 
     array = events.arrays(library='ak')
 
@@ -17,8 +23,6 @@ with uproot.open(filename) as f:
             print(f'  {key}: {array[key][i]}')
     
     # check distribution of 0 and 1 labels
-    target = ak.to_numpy(array['_label_'])
-    unique, counts = np.unique(target, return_counts=True)
-    distribution = dict(zip(unique, counts))
+    
 
     print("Class distribution:", distribution)
--- reweighting.py
+++ reweighting.py
@@ -1,21 +1,59 @@
 import uproot
 import numpy as np
 import awkward as ak
-import os
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
 
-number = 5
-predictions_filename = f'optuna_outputs/optuna_model_{number}_predictions.root'
 
-def open_root_file(filename):
-    with uproot.open(filename) as file:
-        events = file["Events"]
-        array = events.arrays(library='ak')
-    return array
+optuna_version = 3  # Change this to the version of optuna run
+number_of_models = 10  # Total number of models trained in that optuna run
+sample_number = 1  # Change this to the sample number you want to plot
 
-def reweight():
-    array = open_root_file(predictions_filename)
-    predictions = ak.to_numpy(array['label_sample1'])
-    scores = ak.to_numpy(array['score_label_sample1'])
-    target = ak.to_numpy(array['_label_']).astype(int)
-    predictions_2 = ak.to_numpy(array['label_sample2'])
+for num in range(number_of_models):
+    print('-----------------------------------------------------------')
+    print(f"Processing model {num} from optuna version {optuna_version}")
+    root_file_path = f"optuna_outputs_{optuna_version}/optuna_model_{num}_predictions.root"
+
+    # Open the ROOT file and access the Events tree
+    with uproot.open(root_file_path) as file:
+        tree = file["Events"]
+        
+        # Load the desired variable sample
+        score = tree[f"score_label_sample{sample_number}"].array(library="np")
+        label = tree[f"label_sample{sample_number}"].array(library="np")
+
+        array = tree.arrays(library='ak')
+        target = ak.to_numpy(array['_label_']).astype(int)
+        # get probabilities 
+        pred_probs = ak.to_numpy(array[f'score_label_sample{sample_number}']) # they go from 0.5 to 1
+
+
+    # Separate the scores based on label
+    score_0 = score[label == 0]
+    score_1 = score[label == 1]
+
+    # reweighting of one class to the chosen target distribution!
+    epsilon = 1e-9
+    weights_0 = score_0 / (1 - score_0 + epsilon)
+
+    #reweighted_score_0 = score_0 * weights_0 
+
+    # Plotting distribution
+    plt.figure(figsize=(8, 6))
+    plt.hist(score_0, bins=50, alpha=0.7, label='Original class 0', color='blue', density=True)
+    plt.hist(score_1, bins=50, alpha=0.5, label='Target class 1', color='red', density=True)
+    #plt.hist(reweighted_score_0, bins=50, alpha=0.6, label='Reweighted label_sample1 = 0', color='green', density=True)
+    plt.hist(score_0, bins=50, label='Reweighted class 0', density=True, 
+         weights=weights_0, 
+         histtype='step',      
+         edgecolor='black',    
+         linewidth=1.5)  
+    plt.xlabel(f"Classifier output sample{sample_number}")
+    plt.ylabel("Density")
+    plt.title(f"Distribution of score_label_sample{sample_number}")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(f"distribution_plots/reweighted_plot_optuna_{optuna_version}_{num}_sample{sample_number}.png")
+    plt.close()
     
